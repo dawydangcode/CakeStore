7ad6c2a0c6b240697f76a3dd0e3ad78025d848c4
dawydangcode <122167896+dawydangcode@users.noreply.github.com>
2025-01-03 22:01:25 +0700

    create project

---
+++ src/main/java/vn/edu/iuh/fit/CakeStore/CakeStoreApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.CakeStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/CakeStore/CakeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.CakeStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
